44c900492514261643c858ece1d291d5f165fa56
Palakpreet-Kaur <palakpreet1553.becse24@chitkara.edu.in>
2025-01-30 15:01:51 +0530

    Solution to Traffic Light Simulation

---
+++ Traffic Light Simulation@@6788dd63490637cfc44e1ba9/code.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+    char color;
+    scanf("%c",&color);
+    switch(color){
+        case 'R':
+        printf("Stop");
+        break;
+        case 'G':
+        printf("Go");
+        break;
+        case 'Y':
+        printf("Slow Down");
+        break;
+        default:
+        printf("Invald input");
+        break;
+    }
+}
